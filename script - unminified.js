a7a4a7a4c234cbdd8413fa4a87182359785b96b6
N7Gh0sTT <127984934+N7Gh0sTT@users.noreply.github.com>
2024-08-08 15:36:11 +0200

    update

---
+++ script - unminified.js
@@ -0,0 +1,398 @@
+let descendantMetadata = [];
+let moduleMetadata = [];
+let weaponMetadata = [];
+let reactorMetadata = [];
+let externalcomponentMetadata = [];
+let statMetadata = [];
+fetch('https://open.api.nexon.com/static/tfd/meta/fr/descendant.json')
+    .then(response => response.json())
+    .then(data => {
+        descendantMetadata = data;
+    })
+    .catch(error => {
+        console.error('Erreur lors du chargement des métadonnées des descendants:', error);
+    });
+fetch('https://open.api.nexon.com/static/tfd/meta/fr/module.json')
+    .then(response => response.json())
+    .then(data => {
+        moduleMetadata = data;
+    })
+    .catch(error => {
+        console.error('Erreur lors du chargement des métadonnées des modules:', error);
+    });
+fetch('https://open.api.nexon.com/static/tfd/meta/fr/weapon.json')
+    .then(response => response.json())
+    .then(data => {
+        weaponMetadata = data;
+    })
+    .catch(error => {
+        console.error('Erreur lors du chargement des métadonnées des armes:', error);
+    });
+fetch('https://open.api.nexon.com/static/tfd/meta/fr/reactor.json')
+    .then(response => response.json())
+    .then(data => {
+        reactorMetadata = data;
+    })
+    .catch(error => {
+        console.error('Erreur lors du chargement des métadonnées des réacteurs:', error);
+    });
+fetch('https://open.api.nexon.com/static/tfd/meta/fr/external-component.json')
+    .then(response => response.json())
+    .then(data => {
+        externalcomponentMetadata = data;
+    })
+    .catch(error => {
+        console.error('Erreur lors du chargement des métadonnées des composants externe:', error);
+    });
+fetch('https://open.api.nexon.com/static/tfd/meta/fr/stat.json')
+    .then(response => response.json())
+    .then(data => {
+        statMetadata = data;
+    })
+    .catch(error => {
+        console.error('Erreur lors du chargement des métadonnées des stats:', error);
+    });
+
+document.getElementById('playerForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const playerName = document.getElementById('playerName').value;
+
+    fetchPlayerId(playerName);
+});
+
+function fetchPlayerId(playerName) {
+    const apiKey = 'test_c8134b23f6fe670e0a610bfa21f740f6950d10496ce88515b9a1c7fa2aded2efefe8d04e6d233bd35cf2fabdeb93fb0d';
+    const url = `https://open.api.nexon.com/tfd/v1/id?user_name=${encodeURIComponent(playerName)}`;
+
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'x-nxopen-api-key': apiKey
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data && data.ouid) {
+                fetchPlayerInfo(data.ouid);
+            } else {
+                document.getElementById('playerInfo').innerText = 'Joueur non trouvé.';
+            }
+        })
+        .catch(error => {
+            console.error('Erreur:', error);
+            document.getElementById('playerInfo').innerText = 'Erreur lors de la récupération de l\'ID du joueur.';
+        });
+}
+
+function fetchPlayerInfo(ouid) {
+    const apiKey = 'test_c8134b23f6fe670e0a610bfa21f740f6950d10496ce88515b9a1c7fa2aded2efefe8d04e6d233bd35cf2fabdeb93fb0d';
+    const basicInfoUrl = `https://open.api.nexon.com/tfd/v1/user/basic?ouid=${ouid}`;
+    const descendantInfoUrl = `https://open.api.nexon.com/tfd/v1/user/descendant?ouid=${ouid}`;
+    const reactorInfoUrl = `https://open.api.nexon.com/tfd/v1/user/reactor?language_code=fr&ouid=${ouid}`;
+    const externalcomponentInfoUrl = `https://open.api.nexon.com/tfd/v1/user/external-component?language_code=fr&ouid=${ouid}`;
+    const weaponUrl = `https://open.api.nexon.com/tfd/v1/user/weapon?language_code=fr&ouid=${ouid}`;
+
+    Promise.all([
+        fetch(basicInfoUrl, {
+            method: 'GET',
+            headers: {
+                'x-nxopen-api-key': apiKey
+            }
+        }).then(response => response.json()),
+        fetch(descendantInfoUrl, {
+            method: 'GET',
+            headers: {
+                'x-nxopen-api-key': apiKey
+            }
+        }).then(response => response.json()),
+        fetch(reactorInfoUrl, {
+            method: 'GET',
+            headers: {
+                'x-nxopen-api-key': apiKey
+            }
+        }).then(response => response.json()),
+        fetch(externalcomponentInfoUrl, {
+            method: 'GET',
+            headers: {
+                'x-nxopen-api-key': apiKey
+            }
+        }).then(response => response.json()),
+        fetch(weaponUrl, {
+            method: 'GET',
+            headers: {
+                'x-nxopen-api-key': apiKey
+            }
+        }).then(response => response.json())
+    ])
+        .then(results => {
+            const basicInfo = results[0];
+            const descendantInfo = results[1];
+            const reactorInfo = results[2];
+            const externalcomponentInfo = results[3];
+            const weaponInfo = results[4];
+            displayPlayerInfo(basicInfo, descendantInfo);
+            displayModules(descendantInfo.module || []);
+            displayReactor(reactorInfo, externalcomponentInfo.external_component || []);
+            displayWeapon(weaponInfo.weapon || []);
+        })
+        .catch(error => {
+            console.error('Erreur:', error);
+            document.getElementById('playerInfo').innerText = 'Erreur lors de la récupération des informations du joueur.';
+        });
+}
+
+function displayPlayerInfo(basicInfo, descendantInfo) {
+    const playerInfoDiv = document.getElementById('playerInfo');
+    const descendantMeta = descendantMetadata.find(meta => meta.descendant_id === descendantInfo.descendant_id);
+    const descendantName = descendantMeta ? descendantMeta.descendant_name : 'Nom inconnu';
+    const descendantImg = descendantMeta ? descendantMeta.descendant_image_url : '';
+
+    playerInfoDiv.innerHTML = `
+                <div class="card mb-4">
+                  <div class="card-body">
+                    <h2 class="card-title">Niveau de maîtrise: ${basicInfo.mastery_rank_level}</h2>
+                    <h2 class="card-title">Descendant actuel:</h2>
+                    <p>${descendantName}</p>
+                    <p><img src='${descendantImg}' alt='Image de ${descendantName}' class="img-fluid" width="200px" height="auto"/></p>
+                    <p><strong>Niveau:</strong> ${descendantInfo.descendant_level}</p>
+                    <p><strong>Capacité de modules:</strong> ${descendantInfo.module_capacity}/${descendantInfo.module_max_capacity}</p>
+                  </div>
+                </div>
+
+            `;
+}
+function displayModules(modules) {
+    const modulesContainer = document.getElementById('modulesContainer');
+
+    const modulesHtml = modules.map(module => {
+        const moduleMeta = moduleMetadata.find(meta => meta.module_id === module.module_id);
+        const moduleName = moduleMeta ? moduleMeta.module_name : 'Nom inconnu';
+        const moduleImageUrl = moduleMeta ? moduleMeta.image_url : '';
+        const moduleStat = moduleMeta && moduleMeta.module_stat ? moduleMeta.module_stat.find(stat => stat.level === module.module_enchant_level) : {};
+        const moduleDescription = moduleStat ? moduleStat.value : 'Description non disponible';
+        const moduleCost = moduleStat ? moduleStat.module_capacity : "-";
+
+        let moduleTierClass = '';
+        switch (moduleMeta.module_tier) {
+            case 'Normal':
+                moduleTierClass = 'blue';
+                break;
+            case 'Rare':
+                moduleTierClass = 'purple';
+                break;
+            case 'Ultime':
+                moduleTierClass = 'yellow';
+                break;
+            case 'Transcendant':
+                moduleTierClass = 'red';
+                break;
+            default:
+                moduleTierClass = '';
+        }
+        let moduleType = moduleMeta.module_type === null ? '-' : moduleMeta.module_type;
+
+        return `
+        <div class="moduleCard" onmouseover="showDescription(event)" onmouseout="hideDescription(event)">
+        <div class="CardModuleInfo">
+            <div class="socketBanner">
+                <img width="20" height="20" src="/img/${moduleMeta.module_socket_type}.png">
+                <span>${moduleCost}</span>
+            </div>
+            <div class="moduleImg ${moduleTierClass}">
+                <img width="80" height="80" alt="${moduleName}"
+                     src="${moduleImageUrl}">
+            </div>
+            <div class="moduleName">
+                <p>${moduleName}</p>
+            </div>
+        </div>
+        <div class="CardModuleType">
+            <p>${moduleType}</p>
+        </div>
+        <div class="moduleDescription">${moduleDescription}</div>
+    </div>
+                `;
+    }).join('');
+
+    modulesContainer.innerHTML = modulesHtml;
+}
+function displayReactor(reactorInfo, external_components) {
+    const reactorInfoDiv = document.getElementById('reactorInfo');
+    const reactorMeta = reactorMetadata.find(meta => meta.reactor_id === reactorInfo.reactor_id);
+    const reactorName = reactorMeta ? reactorMeta.reactor_name : 'Nom inconnu';
+    const reactorImg = reactorMeta ? reactorMeta.image_url : '';
+    const reactorCondition = reactorMeta ? reactorMeta.optimized_condition_type : '';
+    let substatsHTML = '';
+    const reactorLvl = reactorMeta && reactorMeta.reactor_skill_power ? reactorMeta.reactor_skill_power.find(stat => stat.level === reactorInfo.reactor_level) : {};
+    if (reactorLvl && reactorLvl.skill_power_coefficient) {
+        reactorLvl.skill_power_coefficient.forEach(stat => {
+            substatsHTML += `<p>${stat.coefficient_stat_id}: x${stat.coefficient_stat_value}</p>`;
+        });
+    }
+    let additionalStatsHTML = '';
+    reactorInfo.reactor_additional_stat.forEach(stat => {
+        additionalStatsHTML += `<p>${stat.additional_stat_name}: ${stat.additional_stat_value}</p>`;
+    });
+
+    let externalComponentsHTML = '';
+    external_components.forEach(external_component => {
+        const componentMeta = externalcomponentMetadata.find(meta => meta.external_component_id === external_component.external_component_id);
+        const componentName = componentMeta ? componentMeta.external_component_name : 'Nom inconnu';
+        const componentImg = componentMeta ? componentMeta.image_url : '';
+        let mainstatcomponentHTML = '';
+        let additionalcomponentStatsHTML = '';
+        const componentLvl = componentMeta && componentMeta.base_stat ? componentMeta.base_stat.find(stat => stat.level === external_component.external_component_level) : {};
+        if (componentLvl && componentLvl.stat_id) {
+            const statMeta = statMetadata.find(meta => meta.stat_id === componentLvl.stat_id);
+            const statName = statMeta ? statMeta.stat_name : componentLvl.stat_id;
+            mainstatcomponentHTML += `<p>${statName}: ${componentLvl.stat_value}</p>`;
+            external_component.external_component_additional_stat.forEach(stat => {
+                additionalcomponentStatsHTML += `<p>${stat.additional_stat_name}: ${stat.additional_stat_value}</p>`;
+            });
+        }
+
+
+
+        externalComponentsHTML += `
+      <div class="external-component">
+        <h3 class="external-component-title">${componentName}(lv.${external_component.external_component_level})</h3>
+        <p><img src="${componentImg}" class="external-component-img"></p>
+        ${mainstatcomponentHTML}
+        ${additionalcomponentStatsHTML}
+      </div>
+    `;
+    });
+
+    reactorInfoDiv.innerHTML = `
+                <div class="card mb-4">
+                  <div class="card-body d-flex">
+                    <div class="reactor-info">
+                    <h2 class="card-title">${reactorName}(lv.${reactorInfo.reactor_level})</h2>
+                    <p><img src="${reactorImg}"></p>
+                    <p>${reactorCondition}</p>
+                    ${substatsHTML}
+                    ${additionalStatsHTML}
+                    <p>Niveau d'amélioration: ${reactorInfo.reactor_enchant_level}</p>
+                    </div>
+                    <div class="external-components d-flex flex-wrap">
+                    ${externalComponentsHTML}
+                  </div>
+                  </div>
+                </div>
+
+            `;
+}
+
+
+function displayWeapon(weapons) {
+    const weaponInfoDiv = document.getElementById('weaponInfo');
+    let weaponsHTML = '';
+    weapons.forEach(weapon => {
+        const weaponMeta = weaponMetadata.find(meta => meta.weapon_id === weapon.weapon_id);
+        const weaponName = weaponMeta ? weaponMeta.weapon_name : 'Nom inconnu';
+        const weaponImg = weaponMeta ? weaponMeta.image_url : '';
+        const weaponModules = weapon.module || [];
+        let weaponStatsHTML = '';
+        weapon.weapon_additional_stat.forEach(stat => {
+            weaponStatsHTML += `<p>${stat.additional_stat_name}: ${stat.additional_stat_value}</p>`;
+        });
+
+        let modulesHTML = '';
+        weaponModules.forEach(module => {
+            const moduleMeta = moduleMetadata.find(meta => meta.module_id === module.module_id);
+            const moduleName = moduleMeta ? moduleMeta.module_name : 'Nom inconnu';
+            const moduleImageUrl = moduleMeta ? moduleMeta.image_url : '';
+            const moduleStat = moduleMeta && moduleMeta.module_stat ? moduleMeta.module_stat.find(stat => stat.level === module.module_enchant_level) : {};
+            const moduleDescription = moduleMeta ? moduleMeta.description : 'Description non disponible';
+            const moduleSocketType = moduleMeta ? moduleMeta.module_socket_type : 'Inconnu';
+            const moduleCost = moduleStat ? moduleStat.module_capacity : '-';
+
+            let moduleTierClass = '';
+            switch (moduleMeta.module_tier) {
+                case 'Normal':
+                    moduleTierClass = 'blue';
+                    break;
+                case 'Rare':
+                    moduleTierClass = 'purple';
+                    break;
+                case 'Ultime':
+                    moduleTierClass = 'yellow';
+                    break;
+                case 'Transcendant':
+                    moduleTierClass = 'red';
+                    break;
+                default:
+                    moduleTierClass = '';
+            }
+
+            modulesHTML += `
+                <div class="moduleCard">
+                    <div class="CardModuleInfo">
+                        <div class="socketBanner">
+                            <img width="20" height="20" src="/img/${moduleSocketType}.png">
+                            <span>${moduleCost}</span>
+                        </div>
+                        <div class="moduleImg ${moduleTierClass}">
+                            <img width="80" height="80" alt="${moduleName}" src="${moduleImageUrl}">
+                        </div>
+                        <div class="moduleName">
+                            <p>${moduleName}</p>
+                        </div>
+                    </div>
+                    <div class="CardModuleType">
+                        <p>${moduleMeta.module_type || '-'}</p>
+                    </div>
+                    <div class="moduleDescription">${moduleDescription}</div>
+                </div>
+            `;
+        });
+
+
+        weaponsHTML += `
+      <div class="weapon-container">
+                <div class="weapon">
+                    <h3 class="weapon-title">${weaponName}(lv.${weapon.weapon_level})</h3>
+                    <p><img src="${weaponImg}" class="weapon-img"></p>
+                    ${weaponStatsHTML}
+                </div>
+                <div class="weapon-modules">
+                    ${modulesHTML}
+                </div>
+            </div>
+    `;
+    });
+    weaponInfoDiv.innerHTML = weaponsHTML;
+}
+
+
+function showDescription(event) {
+    const moduleCard = event.currentTarget;
+    const description = moduleCard.querySelector('.moduleDescription');
+    description.style.width = '300px';
+    const rect = moduleCard.getBoundingClientRect();
+    const descriptionRect = description.getBoundingClientRect();
+
+
+    if (rect.right + descriptionRect.width > window.innerWidth) {
+        description.style.left = 'auto';
+        description.style.right = '100%';
+        description.style.marginRight = '10px';
+    } else {
+        description.style.left = '100%';
+        description.style.right = 'auto';
+        description.style.marginLeft = '10px';
+    }
+    description.style.visibility = 'visible';
+    description.style.opacity = '1';
+}
+
+function hideDescription(event) {
+    const moduleCard = event.currentTarget;
+    const description = moduleCard.querySelector('.moduleDescription');
+    description.style.left = '100%';
+    description.style.right = 'auto';
+    description.style.marginLeft = '10px';
+    description.style.marginRight = '0';
+    description.style.visibility = 'hidden';
+    description.style.opacity = '0';
+}
---
+++ script.js
@@ -0,0 +1,87 @@
+let descendantMetadata=[],moduleMetadata=[],weaponMetadata=[],reactorMetadata=[],externalcomponentMetadata=[],statMetadata=[];function fetchPlayerId(e){let t=`https://open.api.nexon.com/tfd/v1/id?user_name=${encodeURIComponent(e)}`;fetch(t,{method:"GET",headers:{"x-nxopen-api-key":"test_c8134b23f6fe670e0a610bfa21f740f6950d10496ce88515b9a1c7fa2aded2efefe8d04e6d233bd35cf2fabdeb93fb0d"}}).then(e=>e.json()).then(e=>{e&&e.ouid?fetchPlayerInfo(e.ouid):document.getElementById("playerInfo").innerText="Joueur non trouv\xe9."}).catch(e=>{console.error("Erreur:",e),document.getElementById("playerInfo").innerText="Erreur lors de la r\xe9cup\xe9ration de l'ID du joueur."})}function fetchPlayerInfo(e){let t="test_c8134b23f6fe670e0a610bfa21f740f6950d10496ce88515b9a1c7fa2aded2efefe8d04e6d233bd35cf2fabdeb93fb0d",a=`https://open.api.nexon.com/tfd/v1/user/basic?ouid=${e}`,n=`https://open.api.nexon.com/tfd/v1/user/descendant?ouid=${e}`,o=`https://open.api.nexon.com/tfd/v1/user/reactor?language_code=fr&ouid=${e}`,d=`https://open.api.nexon.com/tfd/v1/user/external-component?language_code=fr&ouid=${e}`,i=`https://open.api.nexon.com/tfd/v1/user/weapon?language_code=fr&ouid=${e}`;Promise.all([fetch(a,{method:"GET",headers:{"x-nxopen-api-key":t}}).then(e=>e.json()),fetch(n,{method:"GET",headers:{"x-nxopen-api-key":t}}).then(e=>e.json()),fetch(o,{method:"GET",headers:{"x-nxopen-api-key":t}}).then(e=>e.json()),fetch(d,{method:"GET",headers:{"x-nxopen-api-key":t}}).then(e=>e.json()),fetch(i,{method:"GET",headers:{"x-nxopen-api-key":t}}).then(e=>e.json())]).then(e=>{let t=e[0],a=e[1],n=e[2],o=e[3],d=e[4];displayPlayerInfo(t,a),displayModules(a.module||[]),displayReactor(n,o.external_component||[]),displayWeapon(d.weapon||[])}).catch(e=>{console.error("Erreur:",e),document.getElementById("playerInfo").innerText="Erreur lors de la r\xe9cup\xe9ration des informations du joueur."})}function displayPlayerInfo(e,t){let a=document.getElementById("playerInfo"),n=descendantMetadata.find(e=>e.descendant_id===t.descendant_id),o=n?n.descendant_name:"Nom inconnu",d=n?n.descendant_image_url:"";a.innerHTML=`
+                <div class="card mb-4">
+                  <div class="card-body">
+                    <h2 class="card-title">Niveau de ma\xeetrise: ${e.mastery_rank_level}</h2>
+                    <h2 class="card-title">Descendant actuel:</h2>
+                    <p>${o}</p>
+                    <p><img src='${d}' alt='Image de ${o}' class="img-fluid" width="200px" height="auto"/></p>
+                    <p><strong>Niveau:</strong> ${t.descendant_level}</p>
+                    <p><strong>Capacit\xe9 de modules:</strong> ${t.module_capacity}/${t.module_max_capacity}</p>
+                  </div>
+                </div>
+
+            `}function displayModules(e){let t=document.getElementById("modulesContainer"),a=e.map(e=>{let t=moduleMetadata.find(t=>t.module_id===e.module_id),a=t?t.module_name:"Nom inconnu",n=t?t.image_url:"",o=t&&t.module_stat?t.module_stat.find(t=>t.level===e.module_enchant_level):{},d=o?o.value:"Description non disponible",i=o?o.module_capacity:"-",l="";switch(t.module_tier){case"Normal":l="blue";break;case"Rare":l="purple";break;case"Ultime":l="yellow";break;case"Transcendant":l="red";break;default:l=""}return`
+        <div class="moduleCard" onmouseover="showDescription(event)" onmouseout="hideDescription(event)">
+        <div class="CardModuleInfo">
+            <div class="socketBanner">
+                <img width="20" height="20" src="/img/${t.module_socket_type}.png">
+                <span>${i}</span>
+            </div>
+            <div class="moduleImg ${l}">
+                <img width="80" height="80" alt="${a}"
+                     src="${n}">
+            </div>
+            <div class="moduleName">
+                <p>${a}</p>
+            </div>
+        </div>
+        <div class="CardModuleType">
+            <p>${null===t.module_type?"-":t.module_type}</p>
+        </div>
+        <div class="moduleDescription">${d}</div>
+    </div>
+                `}).join("");t.innerHTML=a}function displayReactor(e,t){let a=document.getElementById("reactorInfo"),n=reactorMetadata.find(t=>t.reactor_id===e.reactor_id),o=n?n.reactor_name:"Nom inconnu",d=n?n.image_url:"",i=n?n.optimized_condition_type:"",l="",s=n&&n.reactor_skill_power?n.reactor_skill_power.find(t=>t.level===e.reactor_level):{};s&&s.skill_power_coefficient&&s.skill_power_coefficient.forEach(e=>{l+=`<p>${e.coefficient_stat_id}: x${e.coefficient_stat_value}</p>`});let r="";e.reactor_additional_stat.forEach(e=>{r+=`<p>${e.additional_stat_name}: ${e.additional_stat_value}</p>`});let c="";t.forEach(e=>{let t=externalcomponentMetadata.find(t=>t.external_component_id===e.external_component_id),a=t?t.external_component_name:"Nom inconnu",n=t?t.image_url:"",o="",d="",i=t&&t.base_stat?t.base_stat.find(t=>t.level===e.external_component_level):{};if(i&&i.stat_id){let l=statMetadata.find(e=>e.stat_id===i.stat_id),s=l?l.stat_name:i.stat_id;o+=`<p>${s}: ${i.stat_value}</p>`,e.external_component_additional_stat.forEach(e=>{d+=`<p>${e.additional_stat_name}: ${e.additional_stat_value}</p>`})}c+=`
+      <div class="external-component">
+        <h3 class="external-component-title">${a}(lv.${e.external_component_level})</h3>
+        <p><img src="${n}" class="external-component-img"></p>
+        ${o}
+        ${d}
+      </div>
+    `}),a.innerHTML=`
+                <div class="card mb-4">
+                  <div class="card-body d-flex">
+                    <div class="reactor-info">
+                    <h2 class="card-title">${o}(lv.${e.reactor_level})</h2>
+                    <p><img src="${d}"></p>
+                    <p>${i}</p>
+                    ${l}
+                    ${r}
+                    <p>Niveau d'am\xe9lioration: ${e.reactor_enchant_level}</p>
+                    </div>
+                    <div class="external-components d-flex flex-wrap">
+                    ${c}
+                  </div>
+                  </div>
+                </div>
+
+            `}function displayWeapon(e){let t=document.getElementById("weaponInfo"),a="";e.forEach(e=>{let t=weaponMetadata.find(t=>t.weapon_id===e.weapon_id),n=t?t.weapon_name:"Nom inconnu",o=t?t.image_url:"",d=e.module||[],i="";e.weapon_additional_stat.forEach(e=>{i+=`<p>${e.additional_stat_name}: ${e.additional_stat_value}</p>`});let l="";d.forEach(e=>{let t=moduleMetadata.find(t=>t.module_id===e.module_id),a=t?t.module_name:"Nom inconnu",n=t?t.image_url:"",o=t&&t.module_stat?t.module_stat.find(t=>t.level===e.module_enchant_level):{},d=t?t.description:"Description non disponible",i=t?t.module_socket_type:"Inconnu",s=o?o.module_capacity:"-",r="";switch(t.module_tier){case"Normal":r="blue";break;case"Rare":r="purple";break;case"Ultime":r="yellow";break;case"Transcendant":r="red";break;default:r=""}l+=`
+                <div class="moduleCard">
+                    <div class="CardModuleInfo">
+                        <div class="socketBanner">
+                            <img width="20" height="20" src="/img/${i}.png">
+                            <span>${s}</span>
+                        </div>
+                        <div class="moduleImg ${r}">
+                            <img width="80" height="80" alt="${a}" src="${n}">
+                        </div>
+                        <div class="moduleName">
+                            <p>${a}</p>
+                        </div>
+                    </div>
+                    <div class="CardModuleType">
+                        <p>${t.module_type||"-"}</p>
+                    </div>
+                    <div class="moduleDescription">${d}</div>
+                </div>
+            `}),a+=`
+      <div class="weapon-container">
+                <div class="weapon">
+                    <h3 class="weapon-title">${n}(lv.${e.weapon_level})</h3>
+                    <p><img src="${o}" class="weapon-img"></p>
+                    ${i}
+                </div>
+                <div class="weapon-modules">
+                    ${l}
+                </div>
+            </div>
+    `}),t.innerHTML=a}function showDescription(e){let t=e.currentTarget,a=t.querySelector(".moduleDescription");a.style.width="300px";let n=t.getBoundingClientRect(),o=a.getBoundingClientRect();n.right+o.width>window.innerWidth?(a.style.left="auto",a.style.right="100%",a.style.marginRight="10px"):(a.style.left="100%",a.style.right="auto",a.style.marginLeft="10px"),a.style.visibility="visible",a.style.opacity="1"}function hideDescription(e){let t=e.currentTarget,a=t.querySelector(".moduleDescription");a.style.left="100%",a.style.right="auto",a.style.marginLeft="10px",a.style.marginRight="0",a.style.visibility="hidden",a.style.opacity="0"}fetch("https://open.api.nexon.com/static/tfd/meta/fr/descendant.json").then(e=>e.json()).then(e=>{descendantMetadata=e}).catch(e=>{console.error("Erreur lors du chargement des m\xe9tadonn\xe9es des descendants:",e)}),fetch("https://open.api.nexon.com/static/tfd/meta/fr/module.json").then(e=>e.json()).then(e=>{moduleMetadata=e}).catch(e=>{console.error("Erreur lors du chargement des m\xe9tadonn\xe9es des modules:",e)}),fetch("https://open.api.nexon.com/static/tfd/meta/fr/weapon.json").then(e=>e.json()).then(e=>{weaponMetadata=e}).catch(e=>{console.error("Erreur lors du chargement des m\xe9tadonn\xe9es des armes:",e)}),fetch("https://open.api.nexon.com/static/tfd/meta/fr/reactor.json").then(e=>e.json()).then(e=>{reactorMetadata=e}).catch(e=>{console.error("Erreur lors du chargement des m\xe9tadonn\xe9es des r\xe9acteurs:",e)}),fetch("https://open.api.nexon.com/static/tfd/meta/fr/external-component.json").then(e=>e.json()).then(e=>{externalcomponentMetadata=e}).catch(e=>{console.error("Erreur lors du chargement des m\xe9tadonn\xe9es des composants externe:",e)}),fetch("https://open.api.nexon.com/static/tfd/meta/fr/stat.json").then(e=>e.json()).then(e=>{statMetadata=e}).catch(e=>{console.error("Erreur lors du chargement des m\xe9tadonn\xe9es des stats:",e)}),document.getElementById("playerForm").addEventListener("submit",function(e){e.preventDefault();let t=document.getElementById("playerName").value;fetchPlayerId(t)});
